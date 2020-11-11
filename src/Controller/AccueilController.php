8da668cb5345b1c660a0d6bf7efc59d782fb529e
yohann <yohann.durel@gmail.com>
2020-11-11 17:36:00 +0100

    ajout lien BDD again

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Personne;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,11 @@ class AccueilController extends AbstractController
      */
     public function index(): Response
     {
+        $repo = $this->getDoctrine()->getRepository(Personne::class);
+
+        $personne = $repo->findAll();
         return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+            'accueil' => $personne,
         ]);
     }
 }
